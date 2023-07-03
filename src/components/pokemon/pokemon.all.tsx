05f9fabb2bf4272d385c848f70441402d5eb9007
Ronfflex <aiglevisnky@gmail.com>
2023-07-03 22:42:22 +0200

    filters

--- src/components/pokemon/pokemon.all.tsx
+++ src/components/pokemon/pokemon.all.tsx
@@ -1,19 +1,41 @@
-// PokemonList.tsx
 import { useState, useEffect } from "react";
 import { PokemonService } from "../../services";
-import { Pokemons, PokemonsDTO } from "../../dto";
+import { PokemonsDTO, PokemonDTO } from "../../dto";
 import { Link } from "react-router-dom";
 
 const PokemonAll = () => {
   const [pokemonData, setPokemonData] = useState<PokemonsDTO | null>(null);
-  const [searchTerm, setSearchTerm] = useState<string>("");
+  const [pokemonDetails, setPokemonDetails] = useState<PokemonDTO[] | null>(
+    null
+  );
+  const [pokemonTypes, setPokemonTypes] = useState<string[]>([]);
+  const [pokemonAbilities, setPokemonAbilities] = useState<string[]>([]);
+
+  const [searchName, setSearchName] = useState<string>("");
+  const [searchType, setSearchType] = useState<string>("");
+  const [searchAbility, setSearchAbility] = useState<string>("");
+
   const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const data = await PokemonService.getPokemons(150);
         setPokemonData(data);
+
+        setLoading(true);
+        
+        const details = await PokemonService.getPokemonOfPokemons(data);
+        setPokemonDetails(details);
+
+        const types = await PokemonService.getTypes();
+        setPokemonTypes(types);
+
+        const abilities = await PokemonService.getAbilities();
+        setPokemonAbilities(abilities);
+
+        setLoading(false);
       } catch (err: any) {
         setError("Failed to load Pokemon data. Please try again later.");
       }
@@ -23,39 +45,74 @@ const PokemonAll = () => {
   }, []);
 
   const filteredPokemons =
-    pokemonData?.results.filter((pokemon) =>
-      pokemon.name.toLowerCase().includes(searchTerm.toLowerCase().trim())
+    pokemonDetails?.filter(
+      (pokemon) =>
+        pokemon.name.toLowerCase().includes(searchName.toLowerCase().trim()) &&
+        pokemon.types.some((type) =>
+          type.type.name.toLowerCase().includes(searchType.toLowerCase().trim())
+        ) &&
+        pokemon.abilities.some((ability) =>
+          ability.ability.name
+            .toLowerCase()
+            .includes(searchAbility.toLowerCase().trim())
+        )
     ) || [];
 
   if (error)
     return <div className="text-red-600 text-center mt-4">{error}</div>;
-  if (!pokemonData) return <div className="text-center mt-4">Loading...</div>;
+  if (!pokemonData) return <div className="text-center mt-4">Loading Pokemons...</div>;
+  if (loading)
+    return <div className="text-center mt-4">Loading elements...</div>;
 
   return (
     <div className="text-center">
       <input
         type="text"
         placeholder="Search by name..."
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
+        value={searchName}
+        onChange={(e) => setSearchName(e.target.value)}
         className="appearance-none border rounded my-2 mx-4 py-2 pl-3 pr-24 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
       />
 
+      <select
+        value={searchType}
+        onChange={(e) => setSearchType(e.target.value)}
+        className="appearance-none border rounded my-2 mx-4 py-2 pl-3 pr-24 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+      >
+        <option value="">-- select a type --</option>
+        {pokemonTypes.map((type) => (
+          <option key={type} value={type}>
+            {type}
+          </option>
+        ))}
+      </select>
+
+      <select
+        value={searchAbility}
+        onChange={(e) => setSearchAbility(e.target.value)}
+        className="appearance-none border rounded my-2 mx-4 py-2 pl-3 pr-24 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+      >
+        <option value="">-- select an ability --</option>
+        {pokemonAbilities.map((ability) => (
+          <option key={ability} value={ability}>
+            {ability}
+          </option>
+        ))}
+      </select>
+
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
-        {filteredPokemons.map((pokemons: Pokemons) => (
-          <Link to={"/pokemon/" + pokemons.name}>
+        {filteredPokemons.map((pokemon: PokemonDTO) => (
+          <Link to={"/pokemon/" + pokemon.name}>
             <div
-              key={pokemons.name}
+              key={pokemon.name}
               className="flex flex-col items-center justify-center h-64 bg-white rounded-lg shadow-md hover:shadow-lg overflow-hidden cursor-pointer"
             >
               <img
-                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${
-                  pokemons.url.split("/")[pokemons.url.split("/").length - 2]
-                }.png`}
-                alt={pokemons.name}
+                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`}
+                alt={pokemon.name}
                 className="w-32 h-32 mt-4"
               />
-              <h2 className="text-lg font-bold mb-4">{pokemons.name}</h2>
+              <h2 className="text-lg font-bold mb-4">{pokemon.name}</h2>
             </div>
           </Link>
         ))}
--- src/services/pokemon/pokemon.service.ts
+++ src/services/pokemon/pokemon.service.ts
@@ -1,8 +1,10 @@
 import axios from "axios";
 import { PokemonDTO, PokemonsDTO } from "../../dto";
+import { type } from "os";
 
 export class PokemonService {
-  static async getPokemons(num:number): Promise<PokemonsDTO> {
+  /* Get all pokemons */
+  static async getPokemons(num: number): Promise<PokemonsDTO> {
     try {
       const response = await axios.get(
         `https://pokeapi.co/api/v2/pokemon/?limit=${num}`
@@ -17,10 +19,13 @@ export class PokemonService {
     throw new Error("Response data is not in the expected format");
   }
 
+  /* Get a single pokemon */
   static async getPokemon(name: string): Promise<PokemonDTO> {
     try {
-      const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`);
-      if (response.data){
+      const response = await axios.get(
+        `https://pokeapi.co/api/v2/pokemon/${name}`
+      );
+      if (response.data) {
         return response.data;
       }
       throw new Error("Response data is not in the expected format");
@@ -28,5 +33,49 @@ export class PokemonService {
       console.error(`Failed to fetch Pokemon data: ${err.message}`);
       throw err;
     }
+  }
+
+  /* Get pokemon of getPokemons */
+  static async getPokemonOfPokemons(
+    pokemons: PokemonsDTO
+  ): Promise<PokemonDTO[]> {
+    try {
+      const promises = pokemons.results.map(async (pokemon) => {
+        const response = await axios.get(pokemon.url);
+        return response.data;
+      });
+      const results = await Promise.all(promises);
+      return results;
+    } catch (err: any) {
+      console.error(`Failed to fetch Pokemon data: ${err.message}`);
+      throw err;
+    }
+  }
+
+  /* Get pokemon types */
+  static async getTypes(): Promise<string[]> {
+    try {
+      const response = await axios.get("https://pokeapi.co/api/v2/type");
+      if (response.data) {
+        return response.data.results.map((type: any) => type.name);
+      }
+      throw new Error("Response data is not in the expected format");
+    } catch (err: any) {
+      console.error(`Failed to fetch Pokemon types: ${err.message}`);
+      throw err;
+    }
+  }
+
+  /* Get pokemon abilities */
+  static async getAbilities(): Promise<string[]> {
+    const response = await axios.get("https://pokeapi.co/api/v2/ability");
+    if (response.data) {
+      return response.data.results.map((ability: any) => ability.name);
+    }
+    throw new Error("Response data is not in the expected format");
+  }
+  catch(err: any) {
+    console.error(`Failed to fetch Pokemon ability: ${err.message}`);
+    throw err;
   }
 }
